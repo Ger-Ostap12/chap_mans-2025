71538cd8e40fb9f3c00bbd6520b8adca616b31e2
g0sHh-p <egoromashenko@gmail.com>
2025-10-24 23:00:49 +0300

    бдшка

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column,ForeignKey, Integer, String, Boolean, DECIMAL
 from sqlalchemy.orm import relationship
 from sqlalchemy import select
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ import os
 load_dotenv()
  
 DB_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL) # type: ignore
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -20,17 +20,31 @@ class User(Base):
     __tablename__ = "users"
     
     user_id = Column(Integer, primary_key=True)
-    login = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    passwor_hash = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone_number = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+#Модель locations
+class Locations(Base):
+    __tablename__ = "locations"
+    
+    location_id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey("user.users_id"),nullable=False)
+    is_active = Column(Boolean,nullable=False)
+    latitude = Column(DECIMAL(10,8),nullable=False)
+    longitude = Column(DECIMAL(11,8),nullable=False)
+    
+    user = relationship("Users", back_populates="locations")
     
 #создание user
-def create_user(login: str, email: str,passwor_hash: str) -> User:
+def create_user(first_name: str, last_name: str,phone_number: str,password_hash: str) -> User:
     """Создание нового пользователя"""
     new_user = User(
-        login=login,
-        email=email,
-        passwor_hash=passwor_hash
+        first_name = first_name,
+        last_name = last_name,
+        phone_number=phone_number,
+        password_hash=password_hash
     )
         
     session.add(new_user)
@@ -39,26 +53,19 @@ def create_user(login: str, email: str,passwor_hash: str) -> User:
     return new_user
 
 
-#получение данных
+"""#получение данных
 def get_user_by_id(user_id: int):
-    """Получить пользователя по ID"""
+    
     return session.query(User).filter(User.user_id == user_id).first()
 
-def get_user_by_login(login: str):
-    """Получить пользователя по логину"""
-    return session.query(User).filter(User.login == login).first()
-
-def get_user_by_email(email: str):
-    """Получить пользователя по email"""
-    return session.query(User).filter(User.email == email).first()
-
 def get_all_users():
-    """Получить всех пользователей"""
+    
     return session.query(User).all()
 
 
+#обновление данных
 def update_user(user_id: int, **kwargs):
-    """Обновление данных пользователя"""
+    
     user = get_user_by_id(user_id)
     if not user:
         return None
@@ -71,22 +78,14 @@ def update_user(user_id: int, **kwargs):
     return user
 
 
-#обновление данных
-def update_user_email(user_id: int, new_email: str):
-    """Обновление email пользователя"""
-    return update_user(user_id, email=new_email)
-
-def update_user_login(user_id: int, new_login: str):
-    """Обновление логина пользователя"""
-    return update_user(user_id, login=new_login)
-
-
 #удаление пользователя
 def delete_user(user_id: int) -> bool:
-    """Удаление пользователя по ID"""
+    
     user = get_user_by_id(user_id)
     if user:
         session.delete(user)
         session.commit()
         return True
-    return False
+    return False"""
+    
+
